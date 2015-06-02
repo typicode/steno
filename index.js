801a3861d5eda3f95ce25ac0521e41824e1a17d6
Typicode <typicode@gmail.com>
2015-06-02 18:38:07 +0200

    Refactor API and add atomic writing

--- index.js
+++ index.js
@@ -3,59 +3,58 @@ var path = require('path')
 
 var writers = {}
 
-function Writer(filename) {
-  this.filename = filename
-  this._callbacks = []
+// Returns a temporary file
+// Example: for /some/file will return /some/.~file
+function getTempFile(file) {
+  return path.join(path.dirname(file), '.~' + path.basename(file))
 }
 
-Writer.prototype._callback = function(err, data, next) {
-  if (err) throw err
-  next()
-}
-
-Writer.prototype.setCallback = function(cb) {
-  this._callback = cb
-  return this
+function Writer(file) {
+  this.file = file
 }
 
 Writer.prototype.write = function(data, cb) {
 
   if (this.lock) {
-
+    // File is locked
+    // Save data for later
     this.next = data
-    if (cb) this._callbacks.push(cb)
 
   } else {
-
+    // File is not locked
+    // Lock it
     this.lock = true
 
-    var self = this
-    fs.writeFile(this.filename, data, function(err) {
+    // Write data to a temporary file
+    var tmpFile = getTempFile(this.file)
+    fs.writeFile(tmpFile, data, function(err) {
 
-      function next() {
-        self.lock = false
-        if (self.next) {
-          var data = self.next
-          self.next = null
-          self.write(data)
+      if (err) throw err
+
+      // On success rename the temporary file to the real file
+      fs.rename(tmpFile, this.file, function(err) {
+        if (err) throw err
+
+        // Unlock file
+        this.lock = false
+
+        // Write next data if any
+        if (this.next) {
+          var data = this.next
+          this.next = null
+          this.write(data)
         }
-      }
 
-      self._callback(err, data, next)
+      }.bind(this))
 
-      var c
-      while (c = self._callbacks.shift()) {
-        c(err)
-      }
-      if (cb) cb(err)
-    })
+    }.bind(this))
 
   }
 
   return this
 }
 
-module.exports = function(filename) {
-  filename = path.resolve(filename)
-  return writers[filename] = writers[filename] || new Writer(filename)
+module.exports = function(file) {
+  file = path.resolve(file)
+  return writers[file] = writers[file] || new Writer(file)
 }
--- test.js
+++ test.js
@@ -10,7 +10,7 @@ function reset() {
 var max = 10 * 1000 * 1000
 var writer = steno('tmp.txt')
 
-test('writer without callback', function(t) {
+test('should prevent race condition', function(t) {
   reset()
   t.plan(1)
 
@@ -23,63 +23,7 @@ test('writer without callback', function(t) {
   }
 })
 
-test('writer default callback', function(t) {
-  reset()
-  t.plan(2)
-
-  // default callback should call function next
-  // when err is null
-  var err = null
-  var next = function() {
-    t.pass('next was called')
-  }
-  writer._callback(err, '', next)
-
-  // default callback should throw an error
-  t.throws(function() {
-    writer._callback(new Error())
-  })
-})
-
-test('writer with callback', function(t) {
-  reset()
-  t.plan(1)
-
-  writer.setCallback(function(err, data, next) {
-    if (data === max) {
-      t.equal(+fs.readFileSync('tmp.txt'), max)
-    }
-    next()
-  })
-
-  for (var i= 0; i <= max; i++) {
-    writer.write(i)
-  }
-})
-
-test('writer error with callback', function(t) {
-  reset()
-  t.plan(1)
-
-  var writer = steno(__dirname + '/dir/doesnt/exist')
-
-  writer.setCallback(function(err) {
-    t.equal(err.code, 'ENOENT')
-  })
-
-  writer.write('')
-})
-
-test('write callback', function(t) {
-  reset()
-  t.plan(3)
-
-  writer.write('A', t.false)
-  writer.write('B', t.false)
-  writer.write('C', t.false)
-})
-
-test('store absolute paths', function(t) {
+test('should reuse existing writers', function(t) {
   reset()
   t.plan(1)
 
