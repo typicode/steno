b3d58729ac3bc6cc43ae5e57bb21a5145e510fb4
typicode <typicode@gmail.com>
2021-06-13 06:12:46 +0200

    test: update xv

--- src/test.ts
+++ src/test.ts
@@ -2,11 +2,10 @@ import { strictEqual as equal } from 'assert'
 import fs from 'fs'
 import os from 'os'
 import path from 'path'
-import { test } from 'xv'
 
 import { Writer } from './index.js'
 
-await test('steno', async () => {
+export async function testSteno(): Promise<void> {
   const max = 1000
   const dir = fs.mkdtempSync(path.join(os.tmpdir(), 'steno-test-'))
   const file = path.join(dir, 'tmp.txt')
@@ -22,4 +21,4 @@ await test('steno', async () => {
   // All promises should resolve
   await Promise.all(promises)
   equal(parseInt(fs.readFileSync(file, 'utf-8')), max)
-})
+}
