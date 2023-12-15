aa902a0aa26abf288b61d4adc8c011dfc98b8838
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-12-15 12:21:43 -0800

    chore: remove lint changes to reduce PR complexity

--- src/test.ts
+++ src/test.ts
@@ -1,74 +1,29 @@
 import { strictEqual as equal } from 'node:assert'
-import * as fs from 'node:fs'
-import * as os from 'node:os'
-import * as path from 'node:path'
-import * as url from 'node:url'
-
+import fs from 'node:fs'
+import os from 'node:os'
+import path from 'node:path'
+import url from 'node:url'
 import { Writer } from './index.js'
 
-export async function testStenoStrings(): Promise<void> {
-    const max = 1000
+export async function testSteno(): Promise<void> {
+  const max = 1000
 
-    const dir = fs.mkdtempSync(path.join(os.tmpdir(), 'steno-test-'))
+  const dir = fs.mkdtempSync(path.join(os.tmpdir(), 'steno-test-'))
 
-    const file = path.join(dir, 'file.txt')
-    const fileURL = url.pathToFileURL(path.join(dir, 'fileURL.txt'))
+  const file = path.join(dir, 'file.txt')
+  const fileURL = url.pathToFileURL(path.join(dir, 'fileURL.txt'))
 
-    for (const f of [file, fileURL]) {
-        const writer = new Writer(f)
-        const promises = []
+  for (const f of [file, fileURL]) {
+    const writer = new Writer(f)
+    const promises = []
 
-        // Test race condition
-        for (let i = 1; i <= max; ++i) {
-            promises.push(writer.write(String(i)))
-        }
-
-        // All promises should resolve
-        await Promise.all(promises)
-        equal(parseInt(fs.readFileSync(file, 'utf-8')), max)
+    // Test race condition
+    for (let i = 1; i <= max; ++i) {
+      promises.push(writer.write(String(i)))
     }
-}
 
-export async function testStenoUintArrays(): Promise<void> {
-    const max = 1000
-    const dir = fs.mkdtempSync(path.join(os.tmpdir(), 'steno-test-2'))
-
-    const file = path.join(dir, 'file.txt')
-    const fileURL = url.pathToFileURL(path.join(dir, 'fileURL.txt'))
-    for (const f of [file, fileURL]) {
-        const textEncoder = new TextEncoder()
-        const textDecoder = new TextDecoder()
-        const writer = new Writer(f)
-        const promises = []
-
-        // Test race condition
-        for (let i = 1; i <= max; ++i) {
-            promises.push(writer.write(textEncoder.encode(String(i))))
-        }
-
-        // All promises should resolve
-        await Promise.all(promises)
-        equal(parseInt(textDecoder.decode(fs.readFileSync(file))), max)
-    }
-}
-
-export async function testStenoBuffers(): Promise<void> {
-    const max = 1000
-    const dir = fs.mkdtempSync(path.join(os.tmpdir(), 'steno-test-2'))
-
-    const file = path.join(dir, 'file.txt')
-    const fileURL = url.pathToFileURL(path.join(dir, 'fileURL.txt'))
-    for (const f of [file, fileURL]) {
-        const writer = new Writer(f)
-        const promises = []
-
-        // Test race condition
-        for (let i = 1; i <= max; ++i) {
-            promises.push(writer.write(Buffer.from(String(i), 'utf-8')))
-        }
-
-        // All promises should resolve
-        await Promise.all(promises)
-        equal(parseInt(fs.readFileSync(file, 'utf-8')), max)
-    }
+    // All promises should resolve
+    await Promise.all(promises)
+    equal(parseInt(fs.readFileSync(file, 'utf-8')), max)
+  }
 }
